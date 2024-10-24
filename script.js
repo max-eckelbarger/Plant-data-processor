089f099d03679c9a45e34531fbdae226c5f6ea5c
max-eckelbarger <meckelbarger.swcd@gmail.com>
2024-10-24 13:00:58 -0400

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('soilForm').addEventListener('submit', async function(e)
     let pH = document.getElementById('pH').value;
     let moisture = document.getElementById('moisture').value;
 
-    let response = await fetch('https://your-ngrok-url/process', {
+    let response = await fetch('https://4b86-34-82-201-233.ngrok-free.app/process', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ pH: pH, moisture: moisture })
